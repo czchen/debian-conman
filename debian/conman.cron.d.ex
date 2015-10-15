aaa613d176b03775bd2f13625943e136569dca4a
Yao-Po Wang <blue119@gmail.com>
2015-10-16 00:50:05 +0800

    still in tune

--- debian/conman.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the conman package
-#
-0 4	* * *	root	[ -x /usr/bin/conman_maintenance ] && /usr/bin/conman_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(conman):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="conman" command="/usr/bin/conman"
